4212db6d4c11194a0f616d46552a751ecb383414
Nikos Karampas <nikos.karampas@hotmail.com>
2023-04-11 16:23:55 +0300

    Database configuration, ApplicationDbContext, AddDbContext

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }        
+    }
+}
