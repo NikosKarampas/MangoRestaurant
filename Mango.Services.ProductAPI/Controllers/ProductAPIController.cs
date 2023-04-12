2f9f1c7bc6ab4b01fbc36b633e220836eeb9ad11
Nikos Karampas <nikos.karampas@hotmail.com>
2023-04-12 13:34:08 +0300

    ProductAPIController get all products and get product by id endpoints

---
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,53 @@
+﻿using Mango.Services.ProductAPI.Models.DTO;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ProductAPI.Controllers
+{
+    [Route("api/products")]    
+    public class ProductAPIController : ControllerBase
+    {
+        protected ResponseDto _response;
+        private readonly IProductRepository _productRepository;
+
+        public ProductAPIController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ResponseDto>> Get()
+        {
+            try
+            {
+                IEnumerable<ProductDto> productDtos = await _productRepository.GetProducts();
+                _response.Result = productDtos;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ResponseDto>> Get([FromRoute] int id)
+        {
+            try
+            {
+                ProductDto productDto = await _productRepository.GetProductById(id);
+                _response.Result = productDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+
+            return _response;
+        }
+    }
+}
