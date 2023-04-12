f5265f591a3369eb6ea1bba2358d149eab656370
Nikos Karampas <nikos.karampas@hotmail.com>
2023-04-12 15:51:33 +0300

    Add ApiRequest and a base service implementation for http requests (BaseService)

---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,80 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services.IServices;
+using System.Text;
+using System.Text.Json;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        public ResponseDto responseModel { get; set; }
+        public IHttpClientFactory _htppClientFactory { get; set; }
+
+        public BaseService(IHttpClientFactory htppClientFactory)
+        {
+            responseModel = new ResponseDto();
+            _htppClientFactory = htppClientFactory;
+        }
+
+        public async Task<T> SendAsync<T>(ApiRequest apiRequest)
+        {
+            try
+            {
+                var client = _htppClientFactory.CreateClient("MangoAPI");
+                HttpRequestMessage message = new HttpRequestMessage();
+                message.Headers.Add("Accept", "application/json");
+                message.RequestUri = new Uri(apiRequest.Url);
+                client.DefaultRequestHeaders.Clear();
+
+                if (apiRequest.Data != null)
+                {
+                    message.Content = new StringContent(JsonSerializer.Serialize(apiRequest.Data), 
+                        Encoding.UTF8, "application/json");
+                }
+
+                HttpResponseMessage apiResponse = null;
+                switch (apiRequest.ApiType)
+                {
+                    case StaticDetails.ApiType.GET:
+                        message.Method = HttpMethod.Get;
+                        break;
+                    case StaticDetails.ApiType.POST:
+                        message.Method = HttpMethod.Post;
+                        break;
+                    case StaticDetails.ApiType.PUT:
+                        message.Method = HttpMethod.Put;
+                        break;
+                    case StaticDetails.ApiType.DELETE:
+                        message.Method = HttpMethod.Delete;
+                        break;
+                    default:
+                        message.Method = HttpMethod.Get;
+                        break;
+                }
+
+                apiResponse = await client.SendAsync(message);
+
+                var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                var apiResponseDto = JsonSerializer.Deserialize<T>(apiContent);
+                return apiResponseDto;
+            }
+            catch (Exception ex)
+            {
+                var dto = new ResponseDto
+                {
+                    DisplayMessage = "Error",
+                    ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                    IsSuccess = false
+                };
+                var res = JsonSerializer.Serialize(dto);
+                var apiResponseDto = JsonSerializer.Deserialize<T>(res);
+                return apiResponseDto;
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(true);
+        }
+    }
+}
