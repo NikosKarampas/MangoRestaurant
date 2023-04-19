aa8a0f1458d1481163e293f497f9a54d6b7dfe0b
Nikos Karampas <nikos.karampas@hotmail.com>
2023-04-19 18:58:38 +0300

    Change uris for mango client (Mango.Web project)

--- Mango.Services.Identity/StaticDetails.cs
+++ Mango.Services.Identity/StaticDetails.cs
@@ -40,8 +40,8 @@ namespace Mango.Services.Identity
                     ClientId = "mango",
                     ClientSecrets = { new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.Code,
-                    RedirectUris = { "https://localhost:44326/signin-oidc" },
-                    PostLogoutRedirectUris = { "https://localhost:44326/signout-callback-oidc" },
+                    RedirectUris = { "https://localhost:7066/signin-oidc" },
+                    PostLogoutRedirectUris = { "https://localhost:7066/signout-callback-oidc" },
                     AllowedScopes = new List<string>
                     {
                         IdentityServerConstants.StandardScopes.OpenId,
