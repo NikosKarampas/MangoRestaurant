5d854085bc4357ea42cd4c561dc77a3981b56b72
Nikos Karampas <nikos.karampas@hotmail.com>
2023-04-26 21:05:28 +0300

    Add services for ShoppingCartAPI in Program.cs

--- Mango.Services.ProductAPI/Program.cs
+++ Mango.Services.ProductAPI/Program.cs
@@ -48,6 +48,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mango.Services.ProductAPI", Version = "v1" });
     c.EnableAnnotations();
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
