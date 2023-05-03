410bdd279f6eb1b2be08282d05158505b4cda691
Nikos Karampas <nikos.karampas@hotmail.com>
2023-05-03 12:27:31 +0300

    Added CartController functionality

---
+++ Mango.Services.ShoppingCartAPI/Controllers/CartController.cs
@@ -0,0 +1,100 @@
+﻿using Mango.Services.ShoppingCartAPI.Models.DTO;
+using Mango.Services.ShoppingCartAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.ShoppingCartAPI.Controllers
+{
+    [ApiController]
+    [Route("api/cart")]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _cartRepository;
+        protected ResponseDto _response;
+
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+            _response = new ResponseDto();
+        }
+
+        [HttpGet("GetCart/{userId}")]
+        public async Task<ActionResult<object>> GetCart([FromRoute] string userId)
+        {
+            try
+            {
+                CartDto cartDto = await _cartRepository.GetCartByUserId(userId);
+                _response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("AddCart")]
+        public async Task<ActionResult<object>> AddCart([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("UpdateCart")]
+        public async Task<ActionResult<object>> UpdateCart([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                CartDto cartDt = await _cartRepository.CreateUpdateCart(cartDto);
+                _response.Result = cartDt;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpPost("RemoveCart")]
+        public async Task<ActionResult<object>> RemoveCart([FromBody] int cartId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.RemoveFromCart(cartId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        [HttpDelete("ClearCart/{userId}")]
+        public async Task<ActionResult<object>> ClearCart([FromRoute] string userId)
+        {
+            try
+            {
+                bool isSuccess = await _cartRepository.ClearCart(userId);
+                _response.Result = isSuccess;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
--- Mango.Services.ShoppingCartAPI/Program.cs
+++ Mango.Services.ShoppingCartAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using Mango.Services.ShoppingCartAPI;
 using Mango.Services.ShoppingCartAPI.DbContexts;
+using Mango.Services.ShoppingCartAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -16,7 +17,7 @@ IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-//builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICartRepository, CartRepository>();
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
