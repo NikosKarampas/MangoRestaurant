1ffea211ecad1387dce80647518f347510a3a944
Nikos Karampas <nikos.karampas@hotmail.com>
2023-05-05 16:53:38 +0300

    GetDiscountForCode in CouponController

---
+++ Mango.Services.CouponAPI/Controllers/CouponController.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Services.CouponAPI.Models.DTO;
+using Mango.Services.CouponAPI.Repository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mango.Services.CouponAPI.Controllers
+{
+    [ApiController]
+    [Route("api/coupon")]
+    public class CouponController : ControllerBase
+    {
+        private readonly ICouponRepository _couponRepository;
+        protected ResponseDto _response;
+
+        public CouponController(ICouponRepository couponRepository)
+        {
+            _couponRepository = couponRepository;
+            _response = new ResponseDto();
+        }
+
+        [Authorize]
+        [HttpGet("{code}")]
+        public async Task<ActionResult<object>> GetDiscountForCode([FromRoute] string code)
+        {
+            try
+            {
+                CouponDto couponDto = await _couponRepository.GetCouponByCode(code);
+                _response.Result = couponDto;
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+    }
+}
