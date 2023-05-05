2e8c3ef3b8b747c75fdf246a3964a195de2182c9
Nikos Karampas <nikos.karampas@hotmail.com>
2023-05-05 15:26:19 +0300

    Initialize dbcontext, mappingconfig and program class

---
+++ Mango.Services.CouponAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.CouponAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
---
+++ Mango.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+
+namespace Mango.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                //config.CreateMap<Product, ProductDto>().ReverseMap();
+                //config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                //config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                //config.CreateMap<Cart, CartDto>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
