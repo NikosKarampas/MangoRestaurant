07f2fc29d8744841d29c9bc3ca8927b4ce275065
Nikos Karampas <nikos.karampas@hotmail.com>
2023-05-05 16:39:51 +0300

    Seed Coupon database

---
+++ Mango.Services.CouponAPI/Migrations/20230505133840_SeedCouponDatabase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Mango.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCouponDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount" },
+                values: new object[,]
+                {
+                    { 1, "10OFF", 10.0 },
+                    { 2, "20OFF", 20.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+        }
+    }
+}
