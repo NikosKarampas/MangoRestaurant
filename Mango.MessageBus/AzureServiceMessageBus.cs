79d6134adc38d41f37f7c189e5a36544644ef9a1
Nikos Karampas <nikos.karampas@hotmail.com>
2023-05-16 15:06:18 +0300

    AzureServiceMessageBus implements IMessageBus

---
+++ Mango.MessageBus/AzureServiceMessageBus.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.ServiceBus;
+using System.Text.Json;
+
+namespace Mango.MessageBus
+{
+    public class AzureServiceMessageBus : IMessageBus
+    {
+        private string connectionString = "Endpoint=sb://mangorestaurantweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=mxwGqADz37UWxvruxKXkq8Y39wJUA+FTV+ASbNSgx2Q=";
+        private readonly ServiceBusClient _serviceBusClient;        
+
+        public AzureServiceMessageBus()
+        {            
+            _serviceBusClient = new ServiceBusClient(connectionString);            
+        }
+
+        public async Task PublishMessage(BaseMessage message, string topicName)
+        {
+            ServiceBusSender sender = _serviceBusClient.CreateSender(topicName);
+                        
+            var jsonMessage = JsonSerializer.Serialize(message);
+            var serviceBusMessage = new ServiceBusMessage(jsonMessage);
+
+            await sender.SendMessageAsync(serviceBusMessage);
+
+            await sender.CloseAsync();
+        }
+    }
+}
