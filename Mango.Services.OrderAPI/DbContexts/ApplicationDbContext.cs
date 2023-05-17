6d76903f90a7233eb9841259cf86dfeb27c2daea
Nikos Karampas <nikos.karampas@hotmail.com>
2023-05-17 17:14:49 +0300

    Added models, ApplicationDbContext, OrderDetailsConfiguration

---
+++ Mango.Services.OrderAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Mango.Services.OrderAPI.EntityConfigurations;
+using Mango.Services.OrderAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.OrderAPI.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+
+        public DbSet<OrderDetails> OrderDetails { get; set; }        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OrderDetailsConfiguration());            
+        }
+    }
+}
---
+++ Mango.Services.OrderAPI/EntityConfigurations/OrderDetailsConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Mango.Services.OrderAPI.Models;
+
+namespace Mango.Services.OrderAPI.EntityConfigurations
+{
+    public class OrderDetailsConfiguration : IEntityTypeConfiguration<OrderDetails>
+    {
+        public void Configure(EntityTypeBuilder<OrderDetails> builder)
+        {
+            builder.HasKey(k => k.OrderDetailsId);
+
+            builder.HasOne(ch => ch.OrderHeader)
+                .WithMany(cd => cd.OrderDetails)
+                .HasForeignKey(cd => cd.OrderHeaderId)
+                .IsRequired();
+        }
+    }
+}
