05af74b54872f63a9d7b6d693a413ba7c56f85a3
Nikos Karampas <nikos.karampas@hotmail.com>
2023-06-02 17:00:08 +0300

    Add necessary messages and project references

---
+++ Mango.Services.PaymentAPI/Messages/UpdatePaymentResultMessage.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.PaymentAPI.Messages
+{
+    public class UpdatePaymentResultMessage
+    {
+        public int OrderId { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
