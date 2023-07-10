28d67042948475ba9ed0a177ed3d42132f365564
Nikos Karampas <nikos.karampas@hotmail.com>
2023-07-10 18:28:48 +0300

    AddDbContext in Program Email service

--- Mango.Services.Email/Program.cs
+++ Mango.Services.Email/Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var optionBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 optionBuilder.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
