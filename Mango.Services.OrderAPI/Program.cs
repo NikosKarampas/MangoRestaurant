f00f2d32f8b070df932cdbc62f3d6f320012b281
Nikos Karampas <nikos.karampas@hotmail.com>
2023-07-11 13:05:50 +0300

    Mango.Web change service urls to port 5050 pointing to gateway, redirect get all products in gateway

--- Mango.Services.OrderAPI/Program.cs
+++ Mango.Services.OrderAPI/Program.cs
@@ -60,7 +60,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mango.Services.CouponAPI", Version = "v1" });  
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Mango.Services.OrderAPI", Version = "v1" });  
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = @"Enter 'Bearer' [space] and your token",
